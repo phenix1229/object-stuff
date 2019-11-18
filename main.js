5b8f0f5c0ae7f4600beeeac4a965b229bf724939
phenix1229 <paul.garay@codeimmersives.com>
2019-11-18 12:57:44 -0500

    almost done

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 function getFirstName(person) {
+  return person.firstName;
 }
 
 function getLastName(person) {
+  return person.lastName;
 }
 
 function getFullName(person) {
+  return `${person.firstName} ${person.lastName}`;
 }
 
 function setFirstName(person, name) {
+  person.firstName = name;
 }
 
 function setAge(person, age) {
+  person.age = age;
 }
 
 function giveBirthday(person) {
+  if (person.age !== ''){
+    person.age = person.age + 1
+  } else {
+    person.age = 1
+  }
 }
 
 function marry(person1, person2) {
