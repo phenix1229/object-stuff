bd3dcb6685068d8e9d10cd89e659f43784a2cfd2
phenix1229 <paul.garay@codeimmersives.com>
2019-11-18 13:24:30 -0500

    done

--- main.js
+++ main.js
@@ -19,17 +19,25 @@ function setAge(person, age) {
 }
 
 function giveBirthday(person) {
-  if (person.age !== ''){
-    person.age = person.age + 1
-  } else {
+  if ('age' in person === false){
     person.age = 1
+  } else {
+    person.age = person.age + 1
   }
 }
 
 function marry(person1, person2) {
+  person1.married = true;
+  person2.married = true;
+  person1.spouseName = getFullName(person2);
+  person2.spouseName = getFullName(person1);
 }
 
 function divorce(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
 }
 
 module.exports = {
